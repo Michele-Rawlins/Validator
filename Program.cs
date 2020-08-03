6320457eb817a9beeaf76b7659c0ca570fe8a19a
Michele Rawlins <shellbop@att.net>
2020-08-03 13:27:00 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Validator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Please enter your pin number");
+            string pinNumber = Console.ReadLine();
+            if (pinNumber.Length > 3 && pinNumber.Length < 9);
+            bool isValid;
+            int a;
+            isValid = int.TryParse(pinNumber, out a);
+            Console.WriteLine("Pin number is valid:" + isValid);
+
+        }
+    }
+}
